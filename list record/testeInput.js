28242c056ddc138f7b62eacba5efa66fbfae1b77
Caio Rocha <54248474+kayoroxa@users.noreply.github.com>
2021-12-20 17:20:02 -0300

    Update testeInput.js

--- list record/testeInput.js
+++ list record/testeInput.js
@@ -1,57 +1,26 @@
-const array = [
-  {
-    song_Name: 'default',
-    reels: 0000,
-    id: 0000,
-    emAlta: false,
-    problem: false,
-    done: false,
-    link: '',
-  },
-]
+const songsData = []
 // put songs Datas in array
 
 function addToArray(songObject) {
-  if (!songObject.emAlta) {
-    songObject.emAlta = false
-  }
-  if (!songObject.problem) {
-    songObject.problem = false
-  }
-  if (!songObject.done) {
-    songObject.done = false
-  }
-  if (!songObject.id) {
-    songObject.id = 0000
-  }
-  if (!songObject.reels) {
-    songObject.reels = 0000
-  }
-  if (!songObject.link) {
-    songObject.link = ''
-  }
   if (!songObject.song_Name) {
-    console.log('Nem colocasse o nome da musica')
+    throw new Error('song_Name is required')
   }
-  array.push({
+  songsData.push({
     song_Name: songObject.song_Name,
-    reels: songObject.reels,
-    id: songObject.id,
-    emAlta: songObject.emAlta,
-    problem: songObject.problem,
-    done: songObject.done,
-    link: songObject.link,
+    reels: songObject.reels || 0000,
+    id: songObject.id || 0000,
+    emAlta: songObject.emAlta || false,
+    problem: songObject.problem || false,
+    done: songObject.done || false,
+    link: songObject.link || '',
   })
-  return array
 }
 
-const songObject = {
+addToArray({
   song_Name: 'Scars To Your Beautiful',
   id: 2772881,
   problem: true,
   link: 'https://www.letras.mus.br/alessia-cara/scars-to-your-beautiful/',
-}
+})
 
-addToArray(songObject)
-
-console.log(array)
+console.log(songsData)
